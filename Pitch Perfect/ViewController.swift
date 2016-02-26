f5b310128a16eb2d9e118c3fd8ffb2395bfc63a5
Emil <emil@Emils-MacBook-Pro.local>
2016-02-26 06:24:55 +0530

    start recording button, recording text and stop recording button

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: Record the user's voice
+        recordingInProgress.hidden = false
+    }
 
+    @IBAction func stopRecording(sender: UIButton) {
+        //TODO: stop recording when pressed.
+        print("stop recording");
+        recordingInProgress.hidden = true
+    }
 }
 
