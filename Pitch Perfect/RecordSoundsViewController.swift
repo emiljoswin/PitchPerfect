b659177a287db2545a892d85d4baba5858699258
Emil <emil@Emils-MacBook-Pro.local>
2016-03-01 19:54:57 +0530

    renaming the viewcontroller.swift to playsoundsviewcontroller.swift

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RecordSoundsViewController.swift
 //  Pitch Perfect
 //
 //  Created by Emil on 22/02/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecordSoundsViewController: UIViewController {
 
     @IBOutlet weak var recordButton: UIButton!
     
