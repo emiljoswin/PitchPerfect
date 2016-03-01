d220bfcdf32f0129cded9f4946ca84e8115f2035
Emil <emil@Emils-MacBook-Pro.local>
2016-03-01 07:05:23 +0530

    playing an mp3 file in three different ways

---
+++ Pitch Perfect/PlaySoundViewController.swift
@@ -0,0 +1,97 @@
+//
+//  PlaySoundViewController.swift
+//  Pitch Perfect
+//
+//  Created by Emil on 26/02/16.
+//  Copyright © 2016 Emil. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundViewController: UIViewController {
+   
+    var audioPlayer:AVAudioPlayer = AVAudioPlayer()
+    
+    //var player:AVPlayer = AVPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func playSlowAction(sender: UIButton) {
+        print("play slow pressed")
+        
+        // 1. Play a downloaded file from the project.
+        let soundURL: NSURL
+        
+        if let sound = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            soundURL = NSURL(fileURLWithPath: sound)
+          
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOfURL: soundURL)
+                audioPlayer.prepareToPlay()
+                audioPlayer.rate = 0.5
+                audioPlayer.volume = 1.0
+                audioPlayer.play()
+            } catch {
+                print("error fetching the file")
+            }
+
+        } else {
+            print("the file is not found")
+        }
+
+        /*
+        // 2. Locally store the contents of the url into a variable and
+        // then play from there.
+        do {
+            let url = "https://s3.amazonaws.com/udacity-hosted-downloads/ud585/audio/movie_quote.mp3"
+            let fileURL = NSURL(string: url)
+            
+            let soundData = NSData(contentsOfURL: fileURL!)
+            audioPlayer = try AVAudioPlayer(data: soundData!)
+            if (audioPlayer.prepareToPlay()) {
+            
+                audioPlayer.volume = 1.0
+                audioPlayer.rate = 1.0
+                audioPlayer.play()
+            } else {
+                print("failed at prepare to play")
+            }
+        } catch {
+            print("Error getting the audio file")
+        }
+        */
+        
+        /*
+        // 3. Streaming directly from the internet using the url
+        let url = "https://s3.amazonaws.com/udacity-hosted-downloads/ud585/audio/movie_quote.mp3"
+        let fileURL = NSURL(string: url)
+        
+        player = AVPlayer(URL: fileURL!)
+        player.volume = 1.0
+        player.rate = 1.0
+        player.play()
+        */
+    }
+}
--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,13 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    
     @IBOutlet weak var recordingInProgress: UILabel!
     
     @IBOutlet weak var stopButton: UIButton!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("in view did load")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        // This is where we show and hide things
+        print("in view will appeared")
+        stopButton.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,12 +38,16 @@ class ViewController: UIViewController {
     @IBAction func recordAudio(sender: UIButton) {
         //TODO: Record the user's voice
         recordingInProgress.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
     }
 
     @IBAction func stopRecording(sender: UIButton) {
         //TODO: stop recording when pressed.
         print("stop recording");
         recordingInProgress.hidden = true
+        stopButton.hidden = true
+        recordButton.enabled = true
     }
 }
 
